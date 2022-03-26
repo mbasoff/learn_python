559ce1fb2f1952083adda71680e6e59487e729ba
mbasoff <63520680+mbasoff@users.noreply.github.com>
2022-03-26 18:50:14 +0200

    Add files via upload

--- maksym_basov_task_1_hw_5.py
+++ maksym_basov_task_1_hw_5.py
@@ -1,12 +1,15 @@
 # пользователь вводит пароль первый раз система запоминает и просит повторить пароль проверяет
 # его если нет то просит повторить. А если совпал то сообщение.
+import hashlib
+
 user_password: str = input('Введите пароль\n')
-user_password_hash: hash = hash(user_password)
+user_password_hash: hash = hashlib.sha256(user_password.encode())
 user_password_repeat: str = input('Введите пароль еще раз\n')
-user_password_repeat_hash: hash = hash(user_password_repeat)
+user_password_repeat_hash: hash = hashlib.sha256(user_password_repeat_hash.encode())
+
+
 while user_password_hash == user_password_repeat_hash:
     print('Пароль верен')
     break
 else:
     print('Введите пароль еще раз')
-
